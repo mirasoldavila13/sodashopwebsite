5984644f5f0f9eac6af318fc3f1d70eeaa5b9589
mirasoldavila13 <mirasoldavila13@aim.com>
2020-04-27 16:17:29 -0700

    created a User Data Access Objects class

---
+++ SodaShopInc/src/com/sodashop/dao/UserDAO.java
@@ -0,0 +1,50 @@
+package com.sodashop.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+
+import com.sodashop.entity.Users;
+
+public class UserDAO extends JpaDAO<Users> implements GenericDAO<Users> {
+
+	public UserDAO() {
+		
+	}
+	//create delegates to call its superclass; uses the specific entity type USer
+	public Users create(Users user) {
+	 return super.create(user);
+	}
+
+	@Override
+	public Users update(Users user) {
+		return null;
+	}
+
+	@Override
+	public Users get(Object userId) {
+		return null;
+	}
+
+	public Users findByEmail(String email) {
+		return null;
+	}
+
+	@Override
+	public void delete(Object userId) {
+		
+	}
+
+	@Override
+	public List<Users> listAll() {
+		return null;
+	}
+
+	@Override
+	public long count() {
+		return 0;
+	}
+
+}
