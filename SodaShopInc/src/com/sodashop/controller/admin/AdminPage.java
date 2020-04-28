99173574f8ba857ac2b19dac1c94fcf1a2ee7329
mirasoldavila13 <mirasoldavila13@aim.com>
2020-04-28 04:00:00 -0700

    Created Admin Page servlet

---
+++ SodaShopInc/src/com/sodashop/controller/admin/AdminPage.java
@@ -0,0 +1,31 @@
+package com.sodashop.controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/admin/")
+public class AdminPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+ 
+    public AdminPage() {
+        super();
+       
+    }
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String homepage = "index.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(homepage);
+		dispatcher.forward(request, response); 
+	}
+
+
+}
