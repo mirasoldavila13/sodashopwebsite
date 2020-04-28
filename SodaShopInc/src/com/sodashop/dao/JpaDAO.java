baac83b629208a94ceae7a3c59182a655ab4d1bf
mirasoldavila13 <mirasoldavila13@aim.com>
2020-04-27 20:21:26 -0700

    created an update method

--- SodaShopInc/src/com/sodashop/dao/JpaDAO.java
+++ SodaShopInc/src/com/sodashop/dao/JpaDAO.java
@@ -1,5 +1,6 @@
 package com.sodashop.dao;
-import java.util.List;
+
+import java.util.List; 
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -8,32 +9,46 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
+
+
+
 public class JpaDAO<E> {
-private static EntityManagerFactory entityManagerFactory;
-	//purpose of jpaDAO is providing common operations that are shared among the subclasses but it does not implement genericDAO	
-	//dao is subclasses of jpaDAO class
-	static {
-		//EntityManager is injected by the cliend code dao classes
-		entityManagerFactory = Persistence.createEntityManagerFactory("SodaShopInc");
-	}
+	protected static EntityManager entityManager;
+	// purpose of jpaDAO is providing common operations that are shared among the
+	// subclasses but it does not implement genericDAO
+	// dao is subclasses of jpaDAO class
 	
-	public JpaDAO() {
-	}
 	
-	//returns an entity object
+
+	public JpaDAO(EntityManager entityManager) {
+		super();
+		this.entityManager = entityManager;
+	}
+
+	// returns an entity object
 	public E create(E entity) {
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
 		
+		entityManager.getTransaction().begin();
+
 		entityManager.persist(entity);
 		entityManager.flush();
 		entityManager.refresh(entity);
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+		return entity;
+	}
+	
+	public E update(E entity) {
+		entityManager.getTransaction().begin();
+		
+		entity = entityManager.merge(entity);
 		
 		entityManager.getTransaction().commit();
 		entityManager.close();
 		
 		return entity;
 	}
-	
-	
+
 }
