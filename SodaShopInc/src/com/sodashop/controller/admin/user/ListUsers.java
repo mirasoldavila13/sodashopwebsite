945e63c7bd56b8a47ef050a7cf518e606c620c57
mirasoldavila13 <mirasoldavila13@aim.com>
2020-04-29 14:34:06 -0700

    user list servlet

---
+++ SodaShopInc/src/com/sodashop/controller/admin/user/ListUsers.java
@@ -0,0 +1,43 @@
+package com.sodashop.controller.admin.user;
+
+import java.io.IOException; 
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sodashop.entity.Users;
+import com.sodashop.service.UserServ;
+
+/**
+ * Servlet implementation class ListUsers
+ */
+@WebServlet("/admin/list_users")
+public class ListUsers extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public ListUsers() {
+        super();
+        
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UserServ userServ = new UserServ();
+		List<Users> listUsers = userServ.listUser();
+		request.setAttribute("listUsers", listUsers);
+		
+	  
+	   
+	   
+	   String listPage = "user_list.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(listPage);
+		dispatcher.forward(request, response); 
+	}
+
+}
