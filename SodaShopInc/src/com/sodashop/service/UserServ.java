fdadbb1bda39e3555052e3e1fae4142d4bd287e8
mirasoldavila13 <mirasoldavila13@aim.com>
2020-04-30 12:26:30 -0700

    refactorinng

--- SodaShopInc/src/com/sodashop/service/UserServ.java
+++ SodaShopInc/src/com/sodashop/service/UserServ.java
@@ -1,9 +1,15 @@
 package com.sodashop.service;
-import java.io.IOException;
+import java.util.List;
+import java.io.IOException; 
 import javax.persistence.EntityManager; 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 import com.sodashop.dao.UserDAO;
 import com.sodashop.entity.Users;
 
@@ -19,11 +25,33 @@ public class UserServ {
 		// this is the instance of UserDAO class
 		userDAO = new UserDAO(entityManager);
 	}
-
-	public List<Users> listUser() {
+	
+	//this method needs to refresh a list of users
+	public void listUser(HttpServletRequest request, HttpServletResponse response, String message) throws ServletException, IOException {
 		List<Users> listUsers = userDAO.listAll();
+		request.setAttribute("listUsers", listUsers);
 		
-		return listUsers;
+		if(message != null) {
+			request.setAttribute("message", message);
+		}
+		
+		
+		String listPage = "user_list.jsp";
+	   
+		RequestDispatcher dispatcher = request.getRequestDispatcher(listPage);
+		dispatcher.forward(request, response);
+		
+
+	}
+	
+	public void createUser(HttpServletRequest request, HttpServletResponse response){
+		String email = request.getParameter("email");
+		String fullName = request.getParameter("fullName");
+		String password = request.getParameter("password");
+		
+		Users newUser = new Users(email, fullName, password);
+		userDAO.create(newUser);
+		
 		
 	}
 }
