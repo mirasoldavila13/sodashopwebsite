cb651e76eb66bf5f06d70fc45de7991ade7a9956
mirasoldavila13 <mirasoldavila13@aim.com>
2020-04-30 12:31:52 -0700

    sends out a message when a new user has been created by admin

---
+++ SodaShopInc/src/com/sodashop/controller/admin/CreateUser.java
@@ -0,0 +1,28 @@
+package com.sodashop.controller.admin;
+
+import java.io.IOException; 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sodashop.service.UserServ;
+
+
+@WebServlet("/admin/create_user")
+public class CreateUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+
+		UserServ userServ = new UserServ();
+		userServ.createUser(request,response);
+		//after creating the user we want to refresh the page
+		userServ.listUser(request,response, "New User Created Successfully");
+		
+		
+	}
+
+}
