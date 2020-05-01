71bc7afc595a70ec0c1f9979a3b2cad966981747
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-01 14:06:44 -0700

    created a method to find by email. Also Validates

--- SodaShopInc/src/com/sodashop/dao/UserDAO.java
+++ SodaShopInc/src/com/sodashop/dao/UserDAO.java
@@ -31,6 +31,13 @@ public class UserDAO extends JpaDAO<Users> implements GenericDAO<Users> {
 	}
 
 	public Users findByEmail(String email) {
+		List<Users> listUsers =  super.findWithNamedQuery("Users.findByEmail", "email", email);
+		
+		//validate
+		if(listUsers != null && listUsers.size() > 0) {
+			return listUsers.get(0);
+		}
+		
 		return null;
 	}
 
