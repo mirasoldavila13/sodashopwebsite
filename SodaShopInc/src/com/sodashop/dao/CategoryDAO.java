349fcd9cd222a4c232f4d625114745f8ad88b772
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-03 12:10:43 -0700

    refractor

---
+++ SodaShopInc/src/com/sodashop/dao/CategoryDAO.java
@@ -0,0 +1,49 @@
+package com.sodashop.dao;
+
+import java.util.List; 
+
+import javax.persistence.EntityManager;
+import com.sodashop.entity.Category;
+
+public class CategoryDAO extends JpaDAO<Category> implements GenericDAO<Category> {
+	
+	
+	public CategoryDAO(EntityManager entityManager) {
+		super(entityManager);
+	}
+
+	@Override
+	public Category create(Category category) {
+		return super.create(category);
+	}
+
+	@Override
+	public Category update(Category category) {
+		return super.update(category);
+	}
+
+	@Override
+	public Category get(Object id) {
+		return null;
+	}
+
+	@Override
+	public void delete(Object id) {
+		
+	}
+
+	@Override
+	public List<Category> listAll() {
+		return null;
+	}
+
+	@Override
+	public long count() {
+		return 0;
+	}
+
+
+
+	
+
+}
