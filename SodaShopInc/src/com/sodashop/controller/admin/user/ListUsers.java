ef3f8ed59dbcf267b7cf66d22700b6f9273e50bf
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-03 18:41:45 -0700

    list categories servlet

--- SodaShopInc/src/com/sodashop/controller/admin/user/ListUsers.java
+++ SodaShopInc/src/com/sodashop/controller/admin/user/ListUsers.java
@@ -3,6 +3,7 @@ package com.sodashop.controller.admin.user;
 import java.io.IOException;
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,15 +11,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import com.sodashop.controller.admin.Base;
 import com.sodashop.entity.Users;
 import com.sodashop.service.UserServ;
  
-/**
- * Servlet implementation class ListUsers
- */
+
 @WebServlet("/admin/list_users")
-public class ListUsers extends HttpServlet {
+public class ListUsers extends Base {
 	private static final long serialVersionUID = 1L;
        
     
@@ -29,7 +28,7 @@ public class ListUsers extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserServ userServ = new UserServ(request,response);
+		UserServ userServ = new UserServ(entityManager,request,response);
 		userServ.listUser(); 
 	}
 
