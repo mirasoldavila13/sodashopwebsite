c97612d3fae893427ecdbd15a859565ba8240668
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-03 18:43:24 -0700

    extends to the abstract class Base

--- SodaShopInc/src/com/sodashop/controller/admin/user/CreateUser.java
+++ SodaShopInc/src/com/sodashop/controller/admin/user/CreateUser.java
@@ -1,4 +1,4 @@
-package com.sodashop.controller.admin;
+package com.sodashop.controller.admin.user;
 
 import java.io.IOException; 
 import javax.servlet.ServletException;
@@ -7,17 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sodashop.controller.admin.Base;
 import com.sodashop.service.UserServ;
 
 
 @WebServlet("/admin/create_user")
-public class CreateUser extends HttpServlet {
+public class CreateUser extends Base {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 
-		UserServ userServ = new UserServ(request,response);
+		UserServ userServ = new UserServ(entityManager,request,response);
 		userServ.createUser();
 		
 		
