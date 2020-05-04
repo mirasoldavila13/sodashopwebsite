893b9a66c73aef353c293c9cc3847fa47346c658
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-03 18:44:21 -0700

    extends to the abstract class Base

--- SodaShopInc/src/com/sodashop/controller/admin/user/EditUser.java
+++ SodaShopInc/src/com/sodashop/controller/admin/user/EditUser.java
@@ -7,11 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sodashop.controller.admin.Base;
 import com.sodashop.service.UserServ;
 
 
 @WebServlet("/admin/edit_user")
-public class EditUser extends HttpServlet {
+public class EditUser extends Base {
 	private static final long serialVersionUID = 1L;
   
     public EditUser() {
@@ -20,7 +21,7 @@ public class EditUser extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserServ userServ = new UserServ(request, response);
+		UserServ userServ = new UserServ(entityManager,request, response);
 		userServ.editUser();
 	}
 
