6a6ec734573f9f4da20b4e304f7327a66c338432
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-04 00:26:26 -0700

    category

---
+++ SodaShopInc/src/com/sodashop/controller/admin/category/DeleteCategory.java
@@ -0,0 +1,27 @@
+package com.sodashop.controller.admin.category;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/admin/delete_category")
+public class DeleteCategory extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public DeleteCategory() {
+        super();
+      
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
---
+++ SodaShopInc/src/com/sodashop/controller/admin/category/EditCategory.java
@@ -0,0 +1,26 @@
+package com.sodashop.controller.admin.category;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/admin/edit_category")
+public class EditCategory extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+ 
+    public EditCategory() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
