0ee46415dfacb96d46b17be177c901d27f371d29
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-03 18:43:53 -0700

    extends to the abstract class Base

--- SodaShopInc/src/com/sodashop/controller/admin/user/DeleteUser.java
+++ SodaShopInc/src/com/sodashop/controller/admin/user/DeleteUser.java
@@ -7,11 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sodashop.controller.admin.Base;
 import com.sodashop.service.UserServ;
 
 
 @WebServlet("/admin/delete_user")
-public class DeleteUser extends HttpServlet {
+public class DeleteUser extends Base  {
 	private static final long serialVersionUID = 1L;
        
    
@@ -22,7 +23,7 @@ public class DeleteUser extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		UserServ userServ = new UserServ(request, response);
+		UserServ userServ = new UserServ(entityManager,request, response);
 		userServ.deleteUser();
 	}
 
