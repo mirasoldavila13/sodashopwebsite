d8ca0bf5bd2c83a66121a70684486f3879c3029b
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-04 11:00:31 -0700

    edit Category

--- SodaShopInc/src/com/sodashop/controller/admin/category/EditCategory.java
+++ SodaShopInc/src/com/sodashop/controller/admin/category/EditCategory.java
@@ -7,9 +7,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sodashop.controller.admin.Base;
+import com.sodashop.service.CategoryServ;
+
 
 @WebServlet("/admin/edit_category")
-public class EditCategory extends HttpServlet {
+public class EditCategory extends Base {
 	private static final long serialVersionUID = 1L;
  
     public EditCategory() {
@@ -19,8 +22,9 @@ public class EditCategory extends HttpServlet {
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+			CategoryServ categoryServ = new CategoryServ(entityManager,request, response);
+			categoryServ.editCategory();
+
 	}
 
 }
