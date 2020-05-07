1f5b5ba9a8e3c1c19fa7a55ebf2f5adece7cca42
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-07 03:52:35 -0700

    view soda by category servlet

---
+++ SodaShopInc/src/com/sodashop/controller/frontend/ViewSodaByCategory.java
@@ -0,0 +1,28 @@
+package com.sodashop.controller.frontend;
+
+import com.sodashop.controller.Base;
+import com.sodashop.service.SodaServ;
+
+import java.io.IOException;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/view_category")
+public class ViewSodaByCategory extends Base {
+	private static final long serialVersionUID = 1L;
+
+ 
+    public ViewSodaByCategory() {
+       
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		SodaServ sodaServ = new SodaServ(entityManager,request,response);
+		sodaServ.listSodaByCategory();
+	
+	}
+
+}
