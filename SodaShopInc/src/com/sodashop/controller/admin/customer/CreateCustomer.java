7288bc6e1c6ac01949b235b5c8fc01e86766493d
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-08 22:01:25 -0700

    admin customer side

---
+++ SodaShopInc/src/com/sodashop/controller/admin/customer/CreateCustomer.java
@@ -0,0 +1,23 @@
+package com.sodashop.controller.admin.customer;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sodashop.service.CustomerServ;
+
+
+@WebServlet("/admin/create_customer")
+public class CreateCustomer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CustomerServ customerServ = new CustomerServ(request, response);
+		customerServ.createCustomer();
+	}
+
+}
