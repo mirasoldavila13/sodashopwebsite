4b6ef715f663c70d2897203837b1251e10123f4f
mirasoldavila13 <mirasoldavila13@aim.com>
2020-05-08 17:42:41 -0700

    froend end customer servlets and jsps

---
+++ SodaShopInc/WebContent/frontend/message.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>Soda Shop Incorporated Administration</title>
+			<link rel="stylesheet" href="../css/style.css">
+			<script type="text/javascript" src="../js/jquery-3.5.1.min.js"></script>
+			<script type="text/javascript" src="../js/jquery.validate.min.js"></script>
+		</head>
+		<body>
+			<jsp:directive.include file="header.jsp"/>
+			<div align="center">
+				<h3>${message}</h3>
+			</div>
+			<jsp:directive.include file="footer.jsp"/>
+			
+	</body>
+</html>
---
+++ SodaShopInc/src/com/sodashop/controller/frontend/customer/UpdateCustomerProfile.java
@@ -0,0 +1,23 @@
+package com.sodashop.controller.frontend.customer;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sodashop.controller.Base;
+import com.sodashop.service.CustomerServ;
+
+
+@WebServlet("/update_profile")
+public class UpdateCustomerProfile extends Base {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		CustomerServ customerServ = new CustomerServ(entityManager,request, response);
+		customerServ.updateCustomerProfile();
+	}
+
+}
